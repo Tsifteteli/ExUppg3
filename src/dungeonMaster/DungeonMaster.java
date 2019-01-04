d1b4f916b3a372f5eea2331522bb514b954470cf
Tsifteteli <carbol-8@student.ltu.se>
2019-01-04 09:42:07 +0100

    Added introduction()to DungeonMaster.

--- src/dungeonMaster/DungeonMaster.java
+++ src/dungeonMaster/DungeonMaster.java
@@ -8,6 +8,7 @@ package dungeonMaster;
 
 import java.util.Scanner;
 import players.Player;
+import dungeon.Dungeon;
         
 //Klass innehållandes starten och slutet. Saker som sker utanför dungeon:en.
 public class DungeonMaster {
@@ -17,7 +18,7 @@ public class DungeonMaster {
       beginning();
    }
    
-   //börja spelet och skapar player
+   //börja spelet och skapar en player
    private static void beginning() {
       
       System.out.println("Welcomme to Dungeon Master!"); 
@@ -28,6 +29,23 @@ public class DungeonMaster {
       
       Player player1 = new Player(name);
 
-		introducton(player1);
+		introduction(player1);
+   }
+   
+   //introducerar miljön och skapar en dungeon
+   private static void introduction(Player player1) {
+      
+      System.out.println("Welcome brave explorer" + player1.getName());
+      System.out.println("You have arrived to the cave where the lost "
+              + "treasure is supposed to be hidden. "
+              + "You are determined to find it and you enter the cave. "
+              + "Just a few steps in you hear an earth shattering roar! "
+              + "The whole mountain trembles, and the entrance to the "
+              + "cave collapses behind you. "
+              + "That kind of roar can only come from a dragon! "
+              + "You better be careful.");
+      
+      Dungeon smalDungeon = new Dungeon(player1);
+      smalDungeon.playGame();
    }
 }
