3689bec9467b046c7b78e07a376a6ff94930f805
Tsifteteli <carbol-8@student.ltu.se>
2019-01-04 10:15:36 +0100

    Added Player() to Player.

--- src/players/Player.java
+++ src/players/Player.java
@@ -12,9 +12,21 @@ import java.util.ArrayList;
 
 //Klass innehållandes saker rörandes spelaren
 public class Player {
+   
    String name;
    int healthPoints;
    int givesDamage;
    Room position;
    ArrayList<Item> inventory = new ArrayList<Item>();
+   
+   
+   //Konstruktor
+   public Player(String name) {
+      this.name = name;
+      healthPoints = 10;
+      givesDamage = 1; //Onödigt att ge värde här om alla har 1? Sätt till 1 där instansvariabeln deklareras?
+      //position = ??? Hur ska detta skrivas?
+      //inventory ev onödigt att initiera till null här? Är ju ändå tom från början då inget stoppats i den än...?
+   }
+   
 }
