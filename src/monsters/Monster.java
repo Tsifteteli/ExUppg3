73ba71c7d06495e5078985bdc42532908974b66a
Tsifteteli <carbol-8@student.ltu.se>
2019-01-05 16:25:25 +0100

    Added comment and missing curly bracket.

--- src/monsters/Monster.java
+++ src/monsters/Monster.java
@@ -5,51 +5,49 @@ Amanda Nordmark, norama-6@student.ltu.se*/
 
 package monsters;
 
-/**
- *
- * @author Caroline
- */
+//Klass innehållandes information om monster.
 public class Monster {
     
-    //klassvariablerna deklareras
-    private String name;
-    private int healthPoints;
-    private int givesDamage;
-    private String description;
+   //klassvariablerna deklareras
+   private String name;
+   private int healthPoints;
+   private int givesDamage;
+   private String description;
     
     
-    //konstruktorn defineras
-    public Monster (String name, int healthPoints, int givesDamages, String description) {
-        this.name = name;
-        healthPoints = 8;
-        givesDamage = 1;
-        description = description; 
-    }
+   //konstruktorn defineras
+   public Monster (String name, int healthPoints, int givesDamages, String description) {
+      this.name = name;
+      healthPoints = 8;
+      givesDamage = 1;
+      description = description; 
+   }
     
    
 
-    //metod get name
-    public String getName() {
-       return name;
-    }
+   //metod get name
+   public String getName() {
+      return name;
+   }
 
-    //metod set healthPoints
-    public int setHealthPoints () {
-        return healthPoints;
-    }
+   //metod set healthPoints
+   public int setHealthPoints () {
+      return healthPoints;
+   }
 
-    //metod get healthPoints
-    public int getHealthPoints () {
-        return healthPoints;
-    }
+   //metod get healthPoints
+   public int getHealthPoints () {
+      return healthPoints;
+   }
 
-    //metod get givesDamage
-    public int getGivesDamage () {
-         return givesDamage;
-    }
+   //metod get givesDamage
+   public int getGivesDamage () {
+      return givesDamage;
+   }
 
-    //metod get description
-    public String getDescription () {
-           return description; 
+   //metod get description
+   public String getDescription () {
+      return description; 
+   }
 }
 
