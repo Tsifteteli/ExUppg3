d1c44d92c0a726cb10b4727c675672fec3ef20d3
Tsifteteli <carbol-8@student.ltu.se>
2019-01-05 12:35:00 +0100

    Added instance variables and constructor

--- src/dungeon/Room.java
+++ src/dungeon/Room.java
@@ -5,10 +5,31 @@ Amanda Nordmark, norama-6@student.ltu.se*/
 
 package dungeon;
 
-/**
- *
- * @author Caroline
- */
+
+import items.*;
+import monsters.*;
+
+//Klass innehållandes information om och saker som händer i rummen.
 public class Room {
+   
+    private String description;
+    private Item item;
+    private Monster monster;
+    private Door doorNorth;
+    private Door doorEast;
+    private Door doorSouth;
+    private Door doorWest;
+    
+    //Konstruktor
+
+   public Room(String description, Item item, Monster monster, Door doorNorth, Door doorEast, Door doorSouth, Door doorWest) {
+      this.description = description;
+      this.item = item;
+      this.monster = monster;
+      this.doorNorth = doorNorth;
+      this.doorEast = doorEast;
+      this.doorSouth = doorSouth;
+      this.doorWest = doorWest;
+   }
    
 }
