90b5950aaf16a68696c251eeeeffef5871c82e46
Tsifteteli <carbol-8@student.ltu.se>
2019-01-07 17:44:43 +0100

    Fixed text.

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -35,12 +35,12 @@ public class Dungeon {
       Weapon dagger = new Weapon("Dagger", "There is something glimmering in the corner. A pointy dagger is laying on the floor.", 1);
 
       //Skapa monster => (String name, int healthPoints, int givesDamage, String description).
-      Monster scorpilion = new Monster("Scorpiolion", 8, 1, "A huge scorpiolion is pacing back and forth. Three times the size of a normal lion with a scorpion tail.");
-      Monster dragon = new Monster ("Dragon", 18, 1, "There is a greedy and angry dragon guarding the treasure. It roars and spit fire.");
+      Monster scorpiolion = new Monster("Scorpiolion", 8, 1, "A huge scorpiolion is pacing back and forth. Three times the size of a normal lion with a scorpion tail.");
+      Monster dragon = new Monster ("Dragon", 18, 1, "There is a greedy and angry dragon guarding the treasure. It roars and spit fire!");
       
       //Populera dungeon:en med rum inkl innehåll => (String description, Item item, Monster monster, Door doorNorth, Door doorEast, Door doorSouth, Door doorWest).
       dungeon[0][0] = new Room("in a big room lit by torches sitting on the rough cave walls. A dead body with some realy nasty wounds is laying on the floor.", key, null, null, new Door(true), new Door(false),null);
-		dungeon[0][1] = new Room("in another big room lit by torches.", sword, scorpilion, null, new Door(false), new Door(false), new Door(false));
+		dungeon[0][1] = new Room("in another big room lit by torches.", sword, scorpiolion, null, new Door(false), new Door(false), new Door(false));
 		dungeon[0][2] = new Room("outside in the fresh air!", null, null, null, null, null, new Door(false));
 		dungeon[1][0] = new Room("in a small room lit by a few candles sitting on a table in front of you.", null, null, new Door(false), null, new Door(false),null);
 		dungeon[1][1] = new Room("in a smaller room with brick walls lit by candles in candle holders. Piles of sticks, small rocks and miscellaneous dirt is laying in the corners.", potion, null, new Door(false), new Door(true), null, null);
