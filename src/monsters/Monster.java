036b39f6d6e22382a490eb863fb7d7b4de4c4798
Tsifteteli <carbol-8@student.ltu.se>
2019-01-07 17:41:57 +0100

    Fixed comments and spaceing.

--- src/monsters/Monster.java
+++ src/monsters/Monster.java
@@ -8,8 +8,7 @@ package monsters;
 
 //Klass innehållandes information om monster.
 public class Monster {
-    
-   //klassvariablerna deklareras
+   
    private final String name;
    private int healthPoints;
    private final int givesDamage;
@@ -24,27 +23,27 @@ public class Monster {
       this.description = description;   
    }
   
-   //Returnerar name
+   //Returnerar name.
    public String getName() {
       return name;
    }
 
-   //Anger nytt värde på healtPoints 
+   //Anger nytt värde på healtPoints. 
    public void setHealthPoints(int healthPoints) {
        this.healthPoints = healthPoints;
    }
 
-   //Returnerar healthPoints
+   //Returnerar healthPoints.
    public int getHealthPoints() {
       return healthPoints;
    }
 
-   //ReturnerargivesDamage
+   //Returnerar givesDamage.
    public int getGivesDamage() {
       return givesDamage;
    }
 
-   //Returnerar description
+   //Returnerar description.
    public String getDescription() {
       return description; 
    }
